3f176c7db1f7cde3e5448ec9483ed12d3c535f45
Chinenye Raphael <cudraphael@gmail.com>
2021-01-10 22:01:26 +0100

    Implement stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,55 @@
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   var questionIndex = 0;
   var questions = [
     'What\'s your favorite color?',
     'What\'s your favorite animal?',
   ];
   void answerQuestion() {
-    questionIndex = questionIndex + 1;
+    setState(() {
+      questionIndex = questionIndex + 1;
+      print(questionIndex);
+    });
   }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text("This is an app")),
-      body: Column(
-        children: [
-          Text(questions[questionIndex]),
-          RaisedButton(child: Text("Answer 1"), onPressed: answerQuestion),
-          RaisedButton(child: Text("Answer 2"), onPressed: () => print("Answer Chosen")),
-          RaisedButton(child: Text("Answer 3"), onPressed: () {print("Chosen");}),
-
-
-        ]
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("This is an app"),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: () {
+                print("Chosen");
+              },
+            ),
+          ],
+        ),
       ),
-    ),);
+    );
   }
 }
